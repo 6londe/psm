18799b040da6b843fe177995ca5fe029a0df71d9
6londe <6londe@gmail.com>
2018-09-22 10:36:48 +0900

    add SplashActivity

---
+++ app/src/main/java/blonde/psm/SplashActivity.java
@@ -0,0 +1,26 @@
+package blonde.psm;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 800;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
