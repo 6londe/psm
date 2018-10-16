23f4ae88086c795d55eddd10c5455ddcfbc26e32
Changhoon Lee <ch00n.lee@samsung.com>
2018-10-16 11:20:11 +0900

    add emptyView

--- app/src/main/java/blonde/psm/view/detail/DetailActivity.java
+++ app/src/main/java/blonde/psm/view/detail/DetailActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ public class DetailActivity extends BaseActivity {
     private SquareImageView titleImage;
     private TextView titleName, titlePublisher, titleGenre, titlePlatform;
     private ImageView backButton;
+    private Button editButton;
     private Activity activity;
 
     @Override
@@ -50,6 +52,14 @@ public class DetailActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 activity.onBackPressed();
+            }
+        });
+
+        editButton = findViewById(R.id.detail_edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
--- app/src/main/java/blonde/psm/view/search/SearchActivity.java
+++ app/src/main/java/blonde/psm/view/search/SearchActivity.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import blonde.psm.R;
 import blonde.psm.model.helper.FirebaseWrapper;
@@ -19,26 +21,30 @@ public class SearchActivity extends BaseActivity {
 
     private SearchArrayAdapter searchArrayAdapter;
     private FirebaseWrapper firebaseWrapper;
-    private Context context;
+    private Activity activity;
+
+    private TextView emptyView;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        context = this;
+        activity = this;
         firebaseWrapper = new FirebaseWrapper();
         searchArrayAdapter = new SearchArrayAdapter(this, firebaseWrapper.getTitles());
         searchArrayAdapter.clear();
 
         LinearLayout linearLayout = findViewById(R.id.search_activity_layout);
+        linearLayout.setGravity(Gravity.CENTER_HORIZONTAL);
         int dp = (int) getResources().getDisplayMetrics().density;
 
         final SearchEditText searchEditText = new SearchEditText(this, new OnBackPressed() {
             @Override
             public void OnBackPressedCallback() {
                 hideKeyboard();
-                ((Activity) context).onBackPressed();
+                activity.onBackPressed();
             }
         });
         searchEditText.setBackground(getResources().getDrawable(R.drawable.custom_search_bar));
@@ -49,12 +55,10 @@ public class SearchActivity extends BaseActivity {
         searchEditText.setMaxLines(1);
         searchEditText.setSingleLine();
         searchEditText.setCompoundDrawablePadding(10*dp);
-        searchEditText.setHint("Search");
+        searchEditText.setHint(R.string.search_edittext_hint);
         searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -66,20 +70,39 @@ public class SearchActivity extends BaseActivity {
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
+            public void afterTextChanged(Editable editable) { }
         });
-        linearLayout.addView(searchEditText);
+
         searchEditText.requestFocus();
 
-        ListView listView = new ListView(this);
+        listView = new ListView(this);
         LinearLayout.LayoutParams listViewLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         listViewLayoutParams.setMargins(10*dp, 10*dp, 10*dp, 10*dp);
         listView.setLayoutParams(listViewLayoutParams);
         listView.setDivider(null);
         listView.setDividerHeight(0);
         listView.setAdapter(searchArrayAdapter);
+
+        emptyView = new TextView(this);
+        LinearLayout.LayoutParams emptyViewLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        emptyViewLayoutParams.setMargins(10*dp, 40*dp, 10*dp, 10*dp);
+        emptyView.setPadding(10*dp, 5*dp, 10*dp, 5*dp);
+        emptyView.setLayoutParams(emptyViewLayoutParams);
+        emptyView.setText(" Add New + ");
+        emptyView.setTextColor(getResources().getColor(R.color.colorAccent));
+        emptyView.setBackground(getDrawable(R.drawable.custom_rounded_button));
+        emptyView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        emptyView.setVisibility(View.GONE);
+        emptyView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        linearLayout.addView(searchEditText);
+        linearLayout.addView(emptyView);
+        listView.setEmptyView(emptyView);
         linearLayout.addView(listView);
 
         showKeyboard();
@@ -88,14 +111,14 @@ public class SearchActivity extends BaseActivity {
     public void showKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         View v = getCurrentFocus();
-        if (v != null) inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+        if (v != null && inputMethodManager != null) inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
     }
 
     public void hideKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         View view = getCurrentFocus();
         if (view == null) view = new View(this);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        if (inputMethodManager != null) inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     public interface OnBackPressed {
